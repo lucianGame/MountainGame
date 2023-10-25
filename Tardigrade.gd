c673a207903dae9797e58c0fe1c07acab9ded62b
lucymeisler <79884109+lucymeisler@users.noreply.github.com>
2023-10-25 01:37:31 -0700

    enemy ai

--- Tardigrade.gd
+++ Tardigrade.gd
@@ -1,6 +1,8 @@
 extends CharacterBody3D
 
 var player = null
+#var target = null
+
 
 const speed = 2.0
 const attackRange = 2.5
@@ -17,23 +19,29 @@ enum {
 
 @export var player_path : NodePath
 @onready var nav_agent = $NavigationAgent3D
+#@export var target_path : NodePath
 
 @onready var raycast = $RayCast3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = get_node(player_path)
+	#target = get_node(target_path)
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	velocity = Vector3.ZERO
-	nav_agent.set_target_position(player.global_transform.origin) #the enemy will move towards the player's position
 	var next_nav_point = nav_agent.get_next_path_position()
+	
+	velocity = (next_nav_point - global_transform.origin).normalized() * speed
 	
 	
 	if raycast.is_colliding():
 		state = CHASING
+	#else:
+		#state = IDLE
 	
 	
 	match state:
@@ -41,8 +49,8 @@ func _process(delta):
 			test = 1
 			#insert animation here
 		CHASING:
-			test = 2
-			velocity = (next_nav_point - global_transform.origin).normalized() * speed
+			print("hit")
+			nav_agent.set_target_position(player.global_transform.origin) #the enemy will move towards the player's position
 			look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
 			move_and_slide()
 			#insert animation
